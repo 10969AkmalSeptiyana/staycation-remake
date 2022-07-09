b960fcda260bebad146785ddcc52129104ac1a60
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-07-09 15:53:00 +0700

    create button component

---
+++ src/components/ui/Button/__test__/index.test.js
@@ -0,0 +1,34 @@
+import { getByRole, render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Button from "../index";
+
+const MockButton = () => {
+  return (
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+};
+
+describe("Button", () => {
+  it("Should not allowed click button if isDisabled is present", () => {
+    render(<Button isDisabled />);
+    const spanElement = screen.getByTestId("span");
+    expect(spanElement).toBeInTheDocument();
+    expect(spanElement).toHaveClass("disabled");
+  });
+
+  it("Should render loading/spinner", () => {
+    render(<Button isLoading />);
+    const spanElement = screen.getByTestId("span");
+    const buttonElement = screen.getByText(/Loading/i);
+    expect(spanElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it("Should render <Link> component", () => {
+    render(<MockButton />);
+    const anchorElement = screen.getByRole("link");
+    expect(anchorElement).toBeInTheDocument();
+  });
+});
---
+++ src/components/ui/Button/index.js
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.isDisabled || props.isLoading) {
+    if (props.isDisabled) className.push("disabled");
+    return (
+      <span
+        className={className.join(" ")}
+        style={props.style}
+        data-testid="span"
+      >
+        {props.isLoading ? (
+          <>
+            <span className="spinner-border spinner-border-sm mx-5"></span>
+            <span className="sr-only">Loading...</span>
+          </>
+        ) : (
+          props.children
+        )}
+      </span>
+    );
+  }
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" && "_blank"}
+          rel={props.target === "_blank" && "noopener noreferrer"}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
+  return (
+    <button
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.oneOf(["button", "link"]),
+  onClick: PropTypes.func,
+  target: PropTypes.string,
+  href: PropTypes.string,
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isSmall: PropTypes.bool,
+  isLarge: PropTypes.bool,
+  isBlock: PropTypes.bool,
+  isPrimary: PropTypes.bool,
+  isLight: PropTypes.bool,
+  isExternal: PropTypes.bool,
+  hasShadow: PropTypes.bool,
+};
