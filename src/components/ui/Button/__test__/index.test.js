5e32c1be4126ea0059f2d9c2ef9784746fbb4009
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-07-09 15:54:16 +0700

    create button component

--- src/components/ui/Button/__test__/index.test.js
+++ src/components/ui/Button/__test__/index.test.js
@@ -1,4 +1,4 @@
-import { getByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Button from "../index";
 
