721deef8d679b0cd079dedcf332745e5a553719c
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-07-18 10:51:45 +0700

    fix active class header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
+import { useLocation } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 import Logo from "components/Logo/Logo";
 import Button from "components/ui/Button";
 
-export default function Header() {
+export default function Header(props) {
+  const location = useLocation();
+  const getNavLinkClass = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
+
   return (
     <header>
       <Navbar expand="lg">
@@ -12,23 +18,23 @@ export default function Header() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="nav-list ms-auto">
-              <li className="nav-item active">
+              <li className={`nav-item ${getNavLinkClass("/")}`}>
                 <Button className="nav-link" type="link" href="/">
                   Home
                 </Button>
               </li>
-              <li className="nav-item">
-                <Button className="nav-link" type="link" href="/">
+              <li className={`nav-item ${getNavLinkClass("/explore")}`}>
+                <Button className="nav-link" type="link" href="/explore">
                   Explore
                 </Button>
               </li>
-              <li className="nav-item">
-                <Button className="nav-link" type="link" href="/">
+              <li className={`nav-item ${getNavLinkClass("/stories")}`}>
+                <Button className="nav-link" type="link" href="/stories">
                   Stories
                 </Button>
               </li>
-              <li className="nav-item">
-                <Button className="nav-link" type="link" href="/">
+              <li className={`nav-item ${getNavLinkClass("/contact")}`}>
+                <Button className="nav-link" type="link" href="/contact">
                   Contact Us
                 </Button>
               </li>
